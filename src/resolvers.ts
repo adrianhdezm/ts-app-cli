b415c0d1c89c699c1722d822e9653882d615c565
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-11-17 12:47:27 +0100

    Extract common getDevDependencies

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,19 @@
 export const getDevDependencies = (templateName: string): string[] => {
+  const common = [
+    '@types/jest',
+    'jest',
+    'prettier',
+    'ts-jest',
+    'tslint-config-prettier',
+    'tslint',
+    'typescript'
+  ];
   let dependencies: string[] = [];
-
   if (templateName === 'web') {
     dependencies = [
+      ...common,
       '@types/core-js',
-      '@types/jest',
+      '@types/whatwg-fetch',
       'clean-webpack-plugin',
       'css-loader',
       'file-loader',
@@ -12,33 +21,18 @@ export const getDevDependencies = (templateName: string): string[] => {
       'fork-ts-checker-webpack-plugin',
       'html-loader',
       'html-webpack-plugin',
-      'jest',
       'mini-css-extract-plugin',
       'optimize-css-assets-webpack-plugin',
-      'rimraf',
       'style-loader',
       'terser-webpack-plugin',
-      'ts-jest',
       'ts-loader',
       'tsconfig-paths-webpack-plugin',
-      'tslint',
-      'tslint-config-prettier',
-      'typescript',
-      'webpack',
       'webpack-cli',
-      'webpack-dev-server'
+      'webpack-dev-server',
+      'webpack'
     ];
   } else if (templateName === 'node') {
-    dependencies = [
-      'typescript',
-      'tslint',
-      'rimraf',
-      'prettier',
-      'nodemon',
-      'tslint-config-prettier',
-      'cross-env',
-      '@types/node'
-    ];
+    dependencies = [...common, '@types/node', 'cross-env', 'nodemon', 'rimraf'];
   }
 
   return dependencies;
