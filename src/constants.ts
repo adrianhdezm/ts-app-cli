4828076ac5b95e76fc4a4d63b26d9eff8cf69924
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2020-04-10 09:48:49 +0200

    Remove common template files

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,3 @@ import * as path from 'path';
 
 export const CLI_ROOT_PATH = path.join(__dirname, '..');
 export const TEMPLATES_PATH = path.join(CLI_ROOT_PATH, 'templates');
-export const COMMON_TEMPLATE_PATH = path.join(TEMPLATES_PATH, 'common');
